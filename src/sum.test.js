cc27f9e45c59a0c90244bcffe98b09184464e08d
Jean Toledo <jeanctoledo@gmail.com>
2020-06-08 13:37:17 -0300

    test

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,6 +1,6 @@
 import { sum } from "./sum";
 
-test("renders learn react link", () => {
+test("renders learn react link 2", () => {
   expect(true).toEqual(true);
   // expect(sum(2, 2)).toEqual(4);
 });
