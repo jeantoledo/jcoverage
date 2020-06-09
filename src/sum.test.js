37c09f2da7edc270c9eccaab32af75b42d38684a
Jean Toledo <jeanctoledo@gmail.com>
2020-06-09 08:38:42 -0300

    test

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,6 +1,56 @@
-import { sum } from "./sum";
+import {
+  sum,
+  sub,
+  mult,
+  div,
+  isOdd,
+  total,
+  isTrue,
+  isFalse,
+  haveLength,
+} from "./sum";
 
-test("renders learn react link 2", () => {
-  expect(true).toEqual(true);
-  // expect(sum(2, 2)).toEqual(4);
+test("sum", () => {
+  // expect(true).toEqual(true);
+  expect(sum(2, 2)).toEqual(4);
+});
+
+test("sub", () => {
+  // expect(true).toEqual(true);
+  expect(sub(2, 2)).toEqual(0);
+});
+
+test("mult", () => {
+  // expect(true).toEqual(true);
+  expect(mult(2, 2)).toEqual(4);
+});
+
+test("div", () => {
+  // expect(true).toEqual(true);
+  expect(div(2, 2)).toEqual(1);
+});
+
+test("isOdd", () => {
+  // expect(true).toEqual(true);
+  expect(isOdd(3)).toEqual(true);
+});
+
+test("total", () => {
+  // expect(true).toEqual(true);
+  expect(total(2, 2)).toEqual(4);
+});
+
+test("isTrue", () => {
+  // expect(true).toEqual(true);
+  expect(isTrue(true)).toEqual(true);
+});
+
+test("isFalse", () => {
+  // expect(true).toEqual(true);
+  expect(isFalse(true)).toEqual(false);
+});
+
+test("haveLength", () => {
+  // expect(true).toEqual(true);
+  expect(haveLength("true")).toEqual(4);
 });
